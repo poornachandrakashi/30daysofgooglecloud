60d2706b9a242ce117991e8ba117414ab54275e0
hk <45584381+theharshitkumar@users.noreply.github.com>
2020-10-17 17:37:58 +0530

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -61,7 +61,7 @@ def data_saving (biglist):
     with open("my.json","w") as f:
         json.dump(res,f)
     f.close()
-    print(my.json)
+    print(res)
 
 def start_thread(url2):
     threads = 10
